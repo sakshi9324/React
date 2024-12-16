99f1a4a00d70e937cd11cf2c73e90ac0209a5eb6
sakshi9324 <156990357+sakshi9324@users.noreply.github.com>
2024-12-16 12:10:37 +0000

    React importatnt key

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; // it is a core foundational library which is used for taken refrences.
+import ReactDOM from 'react-dom/client'; // reactDom is a implementation of react on WEB like REact Native is a implementation of react on the mobile Application.
+/*both thoese two files react and react-dom is used for manipulate WEB'S DOM.
+React also creates it's own DOM. which is called virtual DOM. after this it's compare to the virtual DOM and main DOM (which is making on the browser) and after replace all the elements/data which is required for complete the process.
+In this ReactDom there is a method which name is createRoot. it is a single Page application (SPA)
+ReactJs is a library of javascript.
+DOM :- DOM is a nothing it is just like a tree structured (elements, tags) which we study in DSA.
+Document Object Model - DOM
+React provide a power which is called JSX. it means we run our html elements through the Javascript. and JSX also look like HTML. 
+In react Js  we create our own tags. like <App>
+Note :- Browser main Dom  render the HTML's page (using root.render) 
+*/
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,7 +2,10 @@
 function App() {
 
   return (
+    <>
    <h1>Start Learning ReactJs with Vite</h1>
+<h2>ReactJs is a library of javascript.</h2>
+</>
   )
 }
 
